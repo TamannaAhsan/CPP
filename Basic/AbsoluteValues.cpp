8a20a2d77ef7ec1827ba499729f282eed984c95e
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2019-07-10 02:28:18 +0600

    Add files via upload

---
+++ Basic/AbsoluteValues.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Enter value: "<<endl;
+    int n;
+    cin>>n;
+
+    if(n>0){
+        cout<<n<<endl;
+    }
+    else if(n<0){
+        cout<<n*(-1)<<endl;
+    }
+}
