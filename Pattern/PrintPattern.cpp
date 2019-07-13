f9454ec9dc19f6b0bf8df6442c45a0047ab02a52
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2019-07-13 17:52:41 +0600

    Add files via upload

---
+++ Pattern/PrintPattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Enter n: "<<endl;
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cout<<"!!!!!!"<<endl;
+    }
+}
